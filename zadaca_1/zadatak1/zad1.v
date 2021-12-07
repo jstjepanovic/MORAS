87a6ac2b37d39de64cdabfd5ddedecd39ddaca46
jstjepanovic <92795129+jstjepanovic@users.noreply.github.com>
2021-12-07 18:04:43 +0100

    Add files via upload

---
+++ zadaca_1/zadatak1/zad1.v
@@ -0,0 +1,17 @@
+Require Import Coq.Bool.Bool.
+Notation " ! b " := (negb b) (at level 0).
+
+(* 1 *)
+(* a *)
+
+Goal forall x y, !(x && y) || (!x && y) || (!x && !y) = !x || !y.
+Proof.
+  destruct x, y ; reflexivity.
+Qed.
+
+(* b *)
+
+Goal forall x y z, !(!x && y && !z) && !(x && y && z) && (x && !y && !z) = x && !y && !z.
+Proof.
+  destruct x, y, z ; reflexivity.
+Qed.
