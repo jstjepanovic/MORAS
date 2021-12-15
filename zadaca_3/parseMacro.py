02709ce16f6f64e8c7713ca25958c3265f3d79f6
jstjepanovic <92795129+jstjepanovic@users.noreply.github.com>
2021-12-15 18:38:51 +0100

    Update parseMacro.py

--- zadaca_3/parseMacro.py
+++ zadaca_3/parseMacro.py
@@ -4,21 +4,26 @@ def _parse_macros(self):
 
 
 def _parse_macro(self, line, p, o):
+    l = line[1:].split(")")
     if line[0] != "$":
         return line
     elif line.split("(")[0] not in self._mac:
         self._flag = False
         self._line = o
         self._errm = "Macro non-existent"
+        return ""
+    elif l[1] != "" or len(l) > 2:
+        self._flag = False
+        self._line = o
+        self._errm = "Code after macro"
+        return ""
     else:
         if line.split("(")[0] == self._mac[0]:
             l = line.split("(")[1].split(",")
             A = l[0]
             B = ""
             for i in l[1]:
-                if i.isdigit():
-                    B = B + i
-                if i.isalpha():
+                if i.isdigit() or i.isalpha():
                     B = B + i
             
             return f"@{A}\nD=M\n@{B}\nM=D"
@@ -28,11 +33,9 @@ def _parse_macro(self, line, p, o):
             A = l[0]
             B = ""
             for i in l[1]:
-                if i.isdigit():
+                if i.isdigit() or i.isalpha():
                     B = B + i
-                if i.isalpha():
-                    B = B + i
-            
+
             return f"@{A}\nD=M\n@swp\nM=D\n@{B}\nD=M\n@{A}\nM=D\n@swp\nD=M\n@{B}\nM=D"
         
         elif line.split("(")[0] == self._mac[2]:
@@ -41,20 +44,16 @@ def _parse_macro(self, line, p, o):
             B = ""
             D = ""
             for i in l[1]:
-                if i.isdigit():
-                    B = B + i
-                if i.isalpha():
+                if i.isdigit() or i.isalpha():
                     B = B + i
             for i in l[2]:
-                if i.isdigit():
+                if i.isdigit() or i.isalpha():
                     D = D + i
-                if i.isalpha():
-                    D = D + i
+
             return f"@{A}\nD=M\n@{D}\nM=D\n@{B}\nD=M\n@{D}\nM=D+M"
 
 def _init_macro(self):
     self._mac = ["$MV","$SWP","$SUM","$WHILE","$END"]
-    
     
     
     
